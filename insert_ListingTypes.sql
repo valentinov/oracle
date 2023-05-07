17db9aad64e0bde6063fca11e4667ff5bdf09e52
valentinov <valentinvecsernik@gmail.com>
2023-05-07 23:13:29 +0200

    Create insert_ListingTypes.sql

---
+++ insert_ListingTypes.sql
@@ -0,0 +1,6 @@
+INSERT INTO ListingTypes VALUES(1,'Apartment');
+INSERT INTO ListingTypes VALUES(2,'Hostel');
+INSERT INTO ListingTypes VALUES(3,'Hotel');
+INSERT INTO ListingTypes VALUES(4,'Guesthouse');
+INSERT INTO ListingTypes VALUES(5,'Student accommodation');
+INSERT INTO ListingTypes VALUES(6,'Vacation Home');
